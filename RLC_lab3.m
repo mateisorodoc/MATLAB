8feea2dc4bd6537c133b4e8cf3c07bf17ec2c911
mateisorodoc <117034706+mateisorodoc@users.noreply.github.com>
2023-11-01 01:50:31 +0200

    Update RLC_lab3.m

--- RLC_lab3.m
+++ RLC_lab3.m
@@ -1,23 +1,25 @@
-
+clear
 syms i R L V
-v = 3.3
-t = [0:0.0001:0.1];
+t = [0:0.0001:0.5];
 
 XC = 0.6283
 XL = 0.0965
 
-I_overall = simplify(dsolve('Di + R/1.8802*i - V/1.8802 = 0', 'i(0) = 0'));
+intensity_overall = simplify(dsolve('Di + (R/1.8802)*i - V/1.8802 = 0', 'i(0) = 0'));
 
-intensity_overall = subs(I, {R, V}, {100, 3.3});
+intensity_overall = subs(intensity_overall, {R, V}, {100, 3.3});
 
 IC = (XL/(XC + XL))*intensity_overall;
+IL = (XC/(XC + XL))*intensity_overall;
 
-dIC = diff(IC);
+dIL = diff(IL);
 
-UC = dIC * (2*10^(-3));
+%UC = UL
+UC = dIL * 0.033;
 
 figure name 'Intensity on C'
 ezplot(IC, t); grid
 
 figure name 'Voltage on C'
 ezplot(UC, t); grid
+
